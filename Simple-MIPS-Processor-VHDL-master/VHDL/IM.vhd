4c36c60f70296a0afd1387ca55d00f51bb881c1a
rohit15033 <81508504+rohit15033@users.noreply.github.com>
2024-05-01 00:58:55 +0700

    Updated IM with Maya's Instruction

--- Simple-MIPS-Processor-VHDL-master/VHDL/IM.vhd
+++ Simple-MIPS-Processor-VHDL-master/VHDL/IM.vhd
@@ -1,21 +1,22 @@
 --**********************
 --*                    *
 --*                    *
---*	   ---         *
---*	--|(0)|---     *
---*	  |---|        *
---*	--|(1)|---     *
+--*       ---         *
+--*    --|(0)|---     *
+--*      |---|        *
+--*    --|(1)|---     *
 --*       |---|        *
---*	--|(2)|-->     *
---*	  |---|        *
+--*    --|(2)|-->     *
+--*      |---|        *
 --*         .          *
 --          .          *
---*	               *
+--*                   *
 --**********************
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.numeric_std.all;
+
 
 library STD;
 use STD.textio.all;
@@ -32,35 +33,23 @@ signal M1: MEMORY := (others => (others => '0'));
 signal D1: std_ulogic_vector(29 downto 0) := (others => '0');
 signal R1: std_ulogic_vector(31 downto 0) := (others => '0');
 begin
-	D1 <= I1(31 downto 2); --PC/4	
+    D1 <= I1(31 downto 2); --PC/4    
 
-	initmem:process
-	file fp: text;
-	variable ln: line;
-	variable instruction: string(1 to 32);
-	variable i, j: integer := 0;
-	variable ch: character := '0';
-	begin
-		file_open(fp, "instruction.txt", READ_MODE);
-		while not endfile(fp) loop
-			readline(fp, ln);
-			read(ln, instruction);
-			for j in 1 to 32 loop
-				ch := instruction(j);
-				if(ch = '0') then
-					M1(i)(32-j) <= '0';
-				else
-					M1(i)(32-j) <= '1';
-				end if;
-			end loop;
-			i := i+1;
-		end loop;
-		file_close(fp);
-		wait;
-	end process;
+	M1(0) <= x"00000000";
+	M1(1) <= x"00000000";
+	M1(2) <= x"00000000";
+	M1(3) <= x"00000000";
+	M1(4) <= x"0AA80000"; --load, first time
+	M1(5) <= x"04010002";
+	M1(6) <= x"0AAA0000"; --load, no stall because the data is already in cache
+	M1(7) <= x"04220002";
+	M1(8) <= x"04430002";
+	M1(9) <= x"08760000"; --load, different data, stall
+	M1(10) <= x"04640002";
+	M1(11) <= x"04850002";
 
-	R1 <= M1(to_integer(unsigned(D1))) when to_integer(unsigned(D1)) < (N-1) else
-	      std_ulogic_vector(to_signed(-1, 32)) when to_integer(unsigned(D1)) > (N-1);
-	
-	O1 <= R1;
+    R1 <= M1(to_integer(unsigned(D1))) when to_integer(unsigned(D1)) < (N-1) else
+          std_ulogic_vector(to_signed(-1, 32)) when to_integer(unsigned(D1)) > (N-1);
+    
+    O1 <= R1;
 end IM1;
